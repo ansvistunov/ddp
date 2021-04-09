dc11529adc3c3e843b4140220c8482141579179e
Alexey Svistunov <ansvistunov@gmail.com>
2021-04-09 20:56:27 +0300

    fix rmi simple

--- practice3/src/main/java/rmi/client/HelloClient.java
+++ practice3/src/main/java/rmi/client/HelloClient.java
@@ -13,12 +13,13 @@ import java.rmi.registry.Registry;
 public class HelloClient {
     public static final int port = 8080;
     public static final String name = "HelloChat";
-    public static final String host = "localhost";
+    public static final String host = "132.145.228.39";
 
     public static void main(String[] args) throws Exception{
         Registry registry = LocateRegistry.getRegistry(host, port);
         System.out.println(String.format("Client connected to registry on host %s and port %d",host, port));
         HelloChat server = (HelloChat) registry.lookup(name);
+        System.out.println("proxy="+server);
         server.message("Alex", "Hello");
     }
 }
