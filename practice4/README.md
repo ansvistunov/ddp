53f87c41a5e1d9e9744bb4992559dfafbf9168a4
Alexey Svistunov <ansvistunov@gmail.com>
2021-04-16 10:16:48 +0300

    fix soap

--- practice4/README.md
+++ practice4/README.md
@@ -1,2 +1,2 @@
 1. start Hello (server)
-2. mvn install (this command generate SOAP client)
+2. mvn clean install (this command generate SOAP client)
--- practice4/src/main/java/hello/HelloClient.java
+++ practice4/src/main/java/hello/HelloClient.java
@@ -7,12 +7,12 @@ import java.net.URL;
 
 public class HelloClient {
     public static final String url = "http://localhost:8080/Hello?wsdl";
-    //http://132.145.228.39:8080/Hello?wsdl
+    //public static final String url = http://132.145.228.39:8080/Hello?wsdl
     public static void main(String[] args) throws Exception{
-        hello.webservice.HelloService helloService = new hello.webservice.HelloService();
+        /*hello.webservice.HelloService helloService = new hello.webservice.HelloService();
         hello.webservice.Hello hello = helloService.getHelloPort();
         System.out.println(hello);
-        System.out.println(hello.sayHello());
+        System.out.println(hello.sayHello());*/
     }
 
 
